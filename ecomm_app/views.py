c78d06165f9a318bfcd18313a55ce996ad063bad
prachiten <prachiten@gmail.com>
2021-09-08 10:50:11 -0700

    search bar implemented

--- ecomm_app/views.py
+++ ecomm_app/views.py
@@ -4,12 +4,17 @@ import os
 
 def index(request):
 
-    settings_dir = os.path.dirname(__file__)
-    PROJECT_ROOT = os.path.abspath(os.path.dirname(settings_dir))    
-    print(PROJECT_ROOT)
+    #settings_dir = os.path.dirname(__file__)
+    #PROJECT_ROOT = os.path.abspath(os.path.dirname(settings_dir))    
+    #print(PROJECT_ROOT)
     context={
         "product_all":Products.objects.all()
     }
+    item_name= request.GET.get('item_name')
+    if (item_name !='' and item_name!=None):
+        context["product_all"]=Products.objects.filter(title__contains=item_name)
+
+    
     return render (request,"ecomm_app/index.html",context)
 
 
