06d2558fddae25a760e8a2d790602b34de78f351
prachiten <prachiten@gmail.com>
2021-09-08 12:31:02 -0700

    pagination implemented

--- ecomm_app/views.py
+++ ecomm_app/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .models import Products
 import os
+from django.core.paginator import Paginator
 
 def index(request):
 
     #settings_dir = os.path.dirname(__file__)
     #PROJECT_ROOT = os.path.abspath(os.path.dirname(settings_dir))    
     #print(PROJECT_ROOT)
+    ################## SEARCH CODE ############################
     context={
         "product_all":Products.objects.all()
     }
@@ -14,6 +16,10 @@ def index(request):
     if (item_name !='' and item_name!=None):
         context["product_all"]=Products.objects.filter(title__contains=item_name)
 
+    ################### PAGINATOR CODE #########################
+    paginator= Paginator(context["product_all"],4)
+    page= request.GET.get('page')
+    context["product_all"]= paginator.get_page(page)
     
     return render (request,"ecomm_app/index.html",context)
 
